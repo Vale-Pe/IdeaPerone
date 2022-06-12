c2508d38b9bb67dd479cf48831a08bf6f0a1a7bc
Valeria Perone <valeperone@gmail.com>
2022-06-12 18:53:04 -0300

    firste commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {<img src={logo} className="App-logo" alt="logo" />}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mi proyecto Javascript
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://proyecto-eligetupropiaaventura.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Elige tu propia aventura
         </a>
       </header>
     </div>
---
+++ src/Caja.js
@@ -0,0 +1,11 @@
+function Caja(props) {
+    return (
+      <div className="Caja">
+          <p>
+            Curso {props.nombre}
+          </p>
+      </div>
+    );
+  }
+  
+  export default Caja;
